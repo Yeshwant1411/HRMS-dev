fb452c9faec741c615cae2afc61564e992655cca
AtsSaikrishna <atssaikrishnavallala@gmail.com>
2024-12-16 17:11:21 +0530

    added document management module

---
+++ DocumentManagementModulee/src/main/java/com/documentManagement/DocumentManagementModuleApplication.java
@@ -0,0 +1,13 @@
+package com.documentManagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocumentManagementModuleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DocumentManagementModuleApplication.class, args);
+	}
+
+}
---
+++ DocumentManagementModulee/src/main/java/com/documentManagement/repository/FileRepository.java
@@ -0,0 +1,12 @@
+package com.documentManagement.repository;
+
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.documentManagement.entity.FileEntity;
+
+@Repository
+public interface FileRepository extends JpaRepository<FileEntity, Long> {
+}
